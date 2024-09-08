b0cc5dee64052424bac95cd9f3494964d4fa7996
yongug <dldyddnr713@gmail.com>
2024-09-08 13:10:16 +0900

    secret info secured

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://dldyddnr7:1234@boilerplate.swjzl.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err))
@@ -11,8 +22,23 @@ mongoose.connect('mongodb+srv://dldyddnr7:1234@boilerplate.swjzl.mongodb.net/?re
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 헬로키티')
 })
+
+app.post('/register', async (req, res) => {
+  // 회원 가입 시 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  try {
+    await user.save(); // await를 사용하여 비동기적으로 데이터를 저장
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.status(400).json({ success: false, err });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
